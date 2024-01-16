4102d736f48e0dd0ef8504e74839706dd390378a
PLucianP <td.lucian@gmail.com>
2024-01-16 10:29:41 +0200

    Adjusted which files should be in the list in s3_object_js and s3_object_json in locals.tf

--- deployments/terraform/7-lwa-app-deployment-pla/locals.tf
+++ deployments/terraform/7-lwa-app-deployment-pla/locals.tf
@@ -1,7 +1,6 @@
 # --- 7-lwa-app-deployment-pla/locals.tf ---
 
 locals {
-
   https_url_environment_js_template = "https://raw.githubusercontent.com/${var.lwa_repo}/${var.lwa_branch}/environment.js.template"
 
   fqdn = var.hostname == "" ? var.dns_domain : join(".", [var.hostname, var.dns_domain])
@@ -13,10 +12,10 @@ locals {
 
   csp_script_src_unsafe_hashes = "'sha256-XkPjGMp0z+c11Qt/zG8pIkC1TIiA9lf9XEXevRQbMTU='"
 
-  csp_connect_src = local.bdns_json_url_specified ? "https://${local.fqdn} ${join(" ", [for url in distinct(regexall("https://[^\"/]+", data.http.bdns_json[0].response_body)) : url])}" : "https://${local.fqdn} ${join(" ", [for url in distinct(regexall("https://[^\"/]+", file(local.bdns_json_local_path))) : url])}"
+  csp_connect_src = local.bdns_json_url_specified ? "${var.bdns_json_url} https://${local.fqdn} ${join(" ", [for url in distinct(regexall("https://[^\"/]+", data.http.bdns_json[0].response_body)) : url])}" : "https://${local.fqdn} ${join(" ", [for url in distinct(regexall("https://[^\"/]+", file(local.bdns_json_local_path))) : url])}"
 
-  s3_object_js   = setsubtract(fileset("${path.module}/LWA", "**/*.js"), ["environment.js", "local_environment.js"])
-  s3_object_json = setsubtract(fileset("${path.module}/LWA", "**/*.json"), ["bdns.json", "package.json", "octopus.json", "lib/zxing-wrapper/package.json"])
+  s3_object_js   = setsubtract(fileset("${path.module}/LWA", "**/*.js"), ["environment.js"])
+  s3_object_json = setsubtract(fileset("${path.module}/LWA", "**/*.json"), ["bdns.json"])
 
   bdns_json_url_specified = var.bdns_json_url == "" ? false : true
   bdns_json_local_path    = "../networks/${var.network_name}/bdns.json"
