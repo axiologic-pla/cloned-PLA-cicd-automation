d5b89b046044844d333885458d40e8e7adb5fc4a
plucianp <td.lucian@gmail.com>
2024-01-23 12:42:08 +0000

    Managed by Terraform - ./private/tst/LWA/tst.platest.eu/terraform

---
+++ private/tst/LWA/tst.platest.eu/terraform/environment.js
@@ -0,0 +1,12 @@
+export default {
+  "epiDomain": "preqa.epi",
+  "epiProtocolVersion": 1,
+  "appBuildVersion":  "LWA2.1",
+  "timePerCall": 10000,
+  "totalWaitTime": 60000,
+  "gto_TimePerCall": 3000,
+  "gto_TotalWaitTime": 15000,
+  "enableEpiDomain": true,
+  "enableRootVersion": true,
+  "bdnsUrl": "https://tst.platest.eu/bdns.json"
+}
